46872e657904c1c74c7ca1e1258114407b78323a
David Sosa <sosa413131@yahoo.com>
2017-12-03 13:51:35 -0500

    added data to schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS BarberShop_db ;
+CREATE DATABASE BarberShop_db;
+use BarberShop_db;
+
+CREATE TABLE barbers(
+
+id int(11) AUTO_INCREMENT NOT NULL,
+barber_name VARCHAR(255) NOT NULL,
+city VARCHAR(255),
+state VARCHAR(2),
+monday BOOLEAN NOT NULL,
+tuesday BOOLEAN NOT NULL,
+wednesday BOOLEAN NOT NULL,
+thursday BOOLEAN NOT NULL,
+friday BOOLEAN NOT NULL,
+saturday BOOLEAN NOT NULL,
+sunday BOOLEAN NOT NULL,
+primary key (id)
+);
+
+
+CREATE TABLE clients(
+
+id int(11) AUTO_INCREMENT NOT NULL,
+client_name VARCHAR(255) NOT NULL,
+city VARCHAR(255),
+state VARCHAR(2),
+primary key (id)
+);
