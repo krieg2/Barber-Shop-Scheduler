fb6c706471a7ed4fac2f58f9f8d3a7d58ccf823a
David Sosa <sosa413131@yahoo.com>
2017-12-03 13:24:18 -0500

    added morgan to log requests to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+var morgan = require('morgan');
 
 var port = process.env.PORT || 3000  ;
 
@@ -21,13 +22,16 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 // Import routes
 var routes = require("./controllers/BarberShop_controller.js");
 
  // give the server access to them.
 app.use("/", routes);
 
+// log all requests to server
+app.use(morgan('tiny'));
+
+// listens for requests
 app.listen(port, function() {
 	console.log("Listening on PORT " + port);
 });
