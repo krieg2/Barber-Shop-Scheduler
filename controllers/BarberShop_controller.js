72eb50e2661da540c0bc8dac149b50f205193cb4
krieg2 <aaronk1@yahoo.com>
2017-12-09 12:50:29 -0500

    installing passport

--- controllers/BarberShop_controller.js
+++ controllers/BarberShop_controller.js
@@ -1,13 +1,24 @@
 const express = require("express");
-
 const router = express.Router();
-
 hbsObject = { dummy: "data"}
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
 
-      res.render("index", hbsObject);
-    });
-  
-  module.exports = router;
+module.exports = function(passport) {
+	// Create all our routes and set up logic within those routes where required.
+	router.get("/", function(req, res) {
+	    res.render("index", hbsObject);
+	});
+
+	router.get("/auth", passport.authenticate("github"));
+	router.get("/auth/error", function(req, res){
+		res.send("Login Failed");
+	});
+	router.get("/auth/callback",
+		passport.authenticate("github", {failureRedirect: "/auth/error"}),
+		function(req, res){
+			res.send("Login success");
+		}
+	);
+
+	return router;
+}
