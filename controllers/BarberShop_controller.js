5570b4ff1a31b541b45244e7e835f73bb82d89b0
krieg2 <aaronk1@yahoo.com>
2017-12-10 14:46:47 -0500

    updating auth routes

--- controllers/BarberShop_controller.js
+++ controllers/BarberShop_controller.js
@@ -1,27 +1,43 @@
 const express = require("express");
 const router = express.Router();
-hbsObject = {
+var hbsObject = {
   dummy: "data"
 }
 
+// Function to ensure that the user has been authenticated
+// before viewing pages.
+const isLoggedIn = function(req, res, next){
+console.log("isLoggedIn:"+req.isAuthenticated());
+console.log("user:"+req.user);
+    if(req.isAuthenticated()){
+    	next();
+    } else{
+    	res.redirect("/");
+    }
+}
 
 // Create all our routes and set up logic within those routes where required.
+// Main page where users can sign in. No authentication required here.
 router.get("/", function (req, res) {
 
-  res.render("index", hbsObject);
-});
-
-router.get("/dashboard/:barberID", function (req, res) {
-  // request to API using barberID
-  res.render("dashboardview", hbsObject);
+    console.log("main page auth:"+req.isAuthenticated());
+    console.log("main page user:"+req.user);
+    res.render("index", {isLoggedIn: req.isAuthenticated()});
 
 });
 
-router.get("/client/:clientID", function (req, res) {
-  // request to API using clientID
+// Request to API using barberID. User must be authenticated.
+router.get("/dashboard/:barberID", isLoggedIn, function (req, res) {
 
-  res.render("clientview", hbsObject);
+    res.render("dashboardview", hbsObject);
+
 });
 
+// Request to API using clientID. User must be authenticated.
+router.get("/client/:clientID", isLoggedIn, function (req, res) {
+
+    res.render("clientview", hbsObject);
+
+});
 
 module.exports = router;
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,28 +3,50 @@ const router = express.Router();
 
 module.exports.setRoutes = function(passport){
 
-    router.get("/google", passport.authenticate("google", {scope: ["profile"]}));
+    router.get("/google", passport.authenticate("google", {scope: ["profile"], session: true}));
     router.get("/google/callback",
         passport.authenticate("google", {failureRedirect: "/error"}),
         function(req, res){
-            console.log("Login success");
-            res.redirect("/client/987654");
+            req.login(req.user, function(err) {
+              if (err) { return next(err); }
+
+              return res.redirect('/client/987654');
+            });
         }
     );
-    router.get("/github", passport.authenticate("github"));
+
+    router.get("/github", passport.authenticate("github", {session: true}));
     router.get("/github/callback",
         passport.authenticate("github", {failureRedirect: "/error"}),
         function(req, res){
-            console.log("Login success");
-            res.redirect("/client/987654");
+            //console.log(req);
+            //console.log(res);
+            //req.login(req.user, function(err) {
+            //  if (err) { return next(err); }
+               res.redirect('/client/987654');
+            //});
         }
+        /*passport.authenticate("github", {failureRedirect: "/error"}),
+        function(req, res){
+            res.redirect("/client/987654");
+        }*/
     );
+
     router.get("/error", function(req, res){
         res.send("Login Failed");
     });
+
     router.get("/logout", function(req, res){
       req.logout();
-      res.redirect("/");
+      req.session.destroy(function(err) {
+        if (err) {
+          console.error(err);
+        } else {
+          res.clearCookie("server-session-cookie-id");
+          res.redirect("/");
+        }
+      });
+
     });
 };
 
