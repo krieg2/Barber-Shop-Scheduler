3bb909e6be91c68b6b8508267f38f011515d2b39
krieg2 <aaronk1@yahoo.com>
2017-12-10 15:12:17 -0500

    remove cookie-parser

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,33 +3,16 @@ const router = express.Router();
 
 module.exports.setRoutes = function(passport){
 
-    router.get("/google", passport.authenticate("google", {scope: ["profile"], session: true}));
+    router.get("/google", passport.authenticate("google", {scope: ["profile"]}));
     router.get("/google/callback",
-        passport.authenticate("google", {failureRedirect: "/error"}),
-        function(req, res){
-            req.login(req.user, function(err) {
-              if (err) { return next(err); }
-
-              return res.redirect('/client/987654');
-            });
-        }
+      passport.authenticate("google",
+        {successRedirect: "/client/987654", failureRedirect: "/error"})
     );
 
-    router.get("/github", passport.authenticate("github", {session: true}));
+    router.get("/github", passport.authenticate("github"));
     router.get("/github/callback",
-        passport.authenticate("github", {failureRedirect: "/error"}),
-        function(req, res){
-            //console.log(req);
-            //console.log(res);
-            //req.login(req.user, function(err) {
-            //  if (err) { return next(err); }
-               res.redirect('/client/987654');
-            //});
-        }
-        /*passport.authenticate("github", {failureRedirect: "/error"}),
-        function(req, res){
-            res.redirect("/client/987654");
-        }*/
+      passport.authenticate("github",
+        {successRedirect: "/client/987654", failureRedirect: "/error"})
     );
 
     router.get("/error", function(req, res){
@@ -39,7 +22,7 @@ module.exports.setRoutes = function(passport){
     router.get("/logout", function(req, res){
       req.logout();
       req.session.destroy(function(err) {
-        if (err) {
+        if (err){
           console.error(err);
         } else {
           res.clearCookie("server-session-cookie-id");
