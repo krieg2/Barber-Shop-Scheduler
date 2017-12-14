4a30aa201d1557bdefdf29224a2154c2bf207fe8
David Sosa <sosa413131@yahoo.com>
2017-12-13 21:02:53 -0500

    appointment js file commented out

--- models/appointment.js
+++ models/appointment.js
@@ -5,7 +5,7 @@ module.exports = (sequelize,DataTypes) => {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      // autoIncrement: true,
+      autoIncrement: true,
       allowNull: false
     },
     appointmentFrom: {
