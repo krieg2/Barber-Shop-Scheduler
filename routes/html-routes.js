935ba14207d3ae1d748dd8b591cc27989b5986d3
krieg2 <aaronk1@yahoo.com>
2017-12-15 14:04:33 -0500

    remove appointments from clientview render

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -52,12 +52,7 @@ router.get("/client/:clientID", function (req, res) {
     //{keyPublishable: keyPublishable}
     db.Business.findAll().then( (businesses) => {
 
-      //for(let i=0; i<businesses.length; i++){
-          //businesses[i].id
-      //}
-      var appointments = ["9:00", "10:00"];
-      res.render("clientview", {businesses: businesses,
-                                appointments: appointments});
+      res.render("clientview", {businesses: businesses});
     });
 
 });
