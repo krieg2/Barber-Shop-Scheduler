ba9bcf6435d5191f6cd5d0a6fc292d15db806e88
hcandelaria <hcandelaria66.7@gmail.com>
2017-12-16 03:29:34 -0500

    created a second test case

--- test/postUser.js
+++ test/postUser.js
@@ -2,7 +2,7 @@ const Nightmare = require('nightmare');
 const expect = require('chai').expect;
 
 
-describe("post a new user", function() {
+describe("barbershop", function() {
 
   this.timeout(30000);
 
@@ -17,11 +17,34 @@ describe("post a new user", function() {
       .type("#signup-password","demo123")
       .click("#customer-type")
       .click("#signup-submit")
+      .wait(1000)
       .evaluate( () =>{
-        return document.title;
-      }).then( title => {
-        expect(title).not.equal("name");
+        return window.location.href;
+      }).then( url => {
+        console.log(url);
+        expect(url).not.equal("http://localhost:3000/auth/error");
         done();
       });
-  });
+    });
+
+    it('should get a error while creating a existing user', done => {
+      Nightmare ({show: true})
+        .goto("http://localhost:3000/")
+        .click("#sign-up")
+        .wait(1000)
+        .type("#signup-firstName","John")
+        .type("#signup-lastName","Smith")
+        .type("#signup-email","johnsmith123@barbershop.com")
+        .type("#signup-password","demo123")
+        .click("#customer-type")
+        .click("#signup-submit")
+        .wait(1000)
+        .evaluate( () =>{
+          return window.location.href;
+        }).then( url => {
+          console.log(url);
+          expect(url).equal("http://localhost:3000/auth/error");
+          done();
+        });
+      });
 });
