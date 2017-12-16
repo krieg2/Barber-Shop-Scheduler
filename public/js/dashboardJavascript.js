5d6c698476b4dcb860fcfb1c9a5aa125c2a86c88
krieg2 <aaronk1@yahoo.com>
2017-12-15 23:06:08 -0500

    fix js

--- public/js/dashboardJavascript.js
+++ public/js/dashboardJavascript.js
@@ -151,47 +151,6 @@ $(document).ready(function () {
         });
 
     });
-// creates the table
-    // $.get("/api/appoiment/user/:id", function (data) {
-
-        console.log(data);
-
-
-        // for (i = 0; i < data.length; i++) {
-        //     if (data.id == barberID){
-        //         console.log("Match!");
-
-                
-        //     };
-
-            // for (i = 0; i < data.Appointments.length; i++) {
-
-            //     barberId = data.Appointments[i].EmployeeId;
-            //     barber = data.id: [barberId]
-            //     haircutPriceId = data.Appointments[i].ServiceId;
-            //     clientFN = data.Appointments[i].;
-            //     clientLN = ;
-
-
-            //     var tableRow = $("<tr>");
-            //     var haircutNumber = $("<td>").append(stringify(number));
-            //     var clientFirstName = $("<td>").append(data.Appointments.firstName);
-            //     var clientLasttName = $("<td>").append(data.Appointments.lastName);
-            //     var rating = $("<td>").append(data.Appointments.rating);
-            //     var feedback = $("<td>").append(data.Appointments.rating);
-
-            //     tableRow.append(haircutNumber);
-            //     tableRow.append(clientFirstName);
-            //     tableRow.append(clientLasttName);
-            //     tableRow.append(rating);
-            //     tableRow.append(feedback);
-            //     $("#appointmentsTableBody").append(tableRow);
-            // }
-        // };
-
-    });
-
-
 
 });
 
